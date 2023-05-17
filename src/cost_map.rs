5fd9ba495abd3b5e633081d03d32b3517978e821
Takashi Ogura <ogura@smilerobotics.com>
2023-05-17 18:44:04 +0900

    dev

--- src/cost_map.rs
+++ src/cost_map.rs
@@ -1,5 +1,4 @@
-use crate::GridMap;
-use crate::Indices;
+use crate::{Indices, GridMap, Cell};
 
 pub fn path_distance_map(map: &GridMap<u8>, path: &[Indices]) -> GridMap<u8> {
     let mut path_distance_map = map.copy_without_value();
@@ -17,14 +16,30 @@ pub fn path_distance_map_internal(map: &mut GridMap<u8>,
     previous_value: u8,
     updated: &mut [bool]) -> bool {
     // all is true, finish.
-    if updated.iter().any(|v| v == false) {
+    if updated.iter().any(|v| *v == false) {
         return true;
     }
+    let current_value = previous_value + 1;
+    let mut current_indices = vec![];
     for ind in previous_indices {
-        let index = map.to_index_by_indices(ind).unwrap();
         // search +/-
-        if !updated[index] {
-            map.cell_by_indices_mut(ind)
-            update[index] = true;
+        let opt_index = map.to_index_by_indices(ind);
+        if opt_index.is_none() {
+            continue;
         }
+        if updated[opt_index.unwrap()] {
+            continue;
+        }
+            for neighbor in ind.neighbors4() {
+                let mut opt_cell = map.cell_by_indices_mut(&neighbor);
+                if opt_cell.is_some() {
+                    let mut cell = opt_cell.unwrap();
+                    cell = Cell::Value(current_value);
+                    let index = map.to_index_by_indices(ind).unwrap();
+                    updated[index] = true;
+                    current_indices.push(neighbor);
+                }
+            }
+    }
+    path_distance_map_internal(map, &current_indices, current_value, updated)
 }
