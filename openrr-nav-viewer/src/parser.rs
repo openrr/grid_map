4cf1c29bb152e44988a32b77adfad9168862a7f6
Katsu Uchiyama <katsu1112uchi@gmail.com>
2023-05-24 19:07:05 +0900

    Add `openrr-nav-viewer`

---
+++ openrr-nav-viewer/src/parser.rs
@@ -0,0 +1,58 @@
+use bevy_egui::egui::{
+    plot::{Line, PlotPoints, Points, Polygon},
+    Color32,
+};
+use grid_map::*;
+use openrr_nav::*;
+
+pub fn parse_grid_map_to_polygon(grid_map: &GridMap<u8>) -> Vec<Polygon> {
+    let min_point = grid_map.min_point();
+    let resolution = grid_map.resolution();
+    let width = grid_map.width();
+    let cells = grid_map.cells();
+
+    let mut polygons = Vec::<Polygon>::new();
+
+    for (i, cell_i) in cells.iter().enumerate() {
+        let order = [0, 1, 3, 2];
+        let plot_points: PlotPoints = order
+            .iter()
+            .map(|j| {
+                let x = min_point.x + (i % width + j % 2) as f64 * resolution;
+                let y = min_point.y + (i / width + j / 2) as f64 * resolution;
+                [x, y]
+            })
+            .collect();
+        let polygon = match cell_i {
+            Cell::Unknown => Polygon::new(plot_points)
+                .color(Color32::from_gray(120))
+                .fill_alpha(1.0),
+            Cell::Value(v) => Polygon::new(plot_points)
+                .color(Color32::from_rgb(*v * 2, 120, 120))
+                .fill_alpha(1.0),
+            _ => Polygon::new(plot_points)
+                .color(Color32::from_gray(0))
+                .fill_alpha(1.0),
+        };
+        polygons.push(polygon);
+    }
+
+    polygons
+}
+
+pub fn parse_robot_path_to_line(robot_path: &RobotPath, color: Color32) -> Line {
+    let plot_points: PlotPoints = robot_path
+        .0
+        .iter()
+        .map(|&p| [p.translation.x, p.translation.y])
+        .collect();
+    Line::new(plot_points).width(10.).color(color)
+}
+
+pub fn parse_robot_pose_to_point(robot_pose: &Pose, color: Color32) -> Points {
+    let x = robot_pose.translation.x;
+    let y = robot_pose.translation.y;
+    let plot_point: PlotPoints = PlotPoints::new(vec![[x, y]]);
+
+    Points::new(plot_point).radius(10.).color(color)
+}
---
+++ openrr-nav/src/robot_path.rs
@@ -0,0 +1,68 @@
+use nalgebra::Isometry2;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Default)]
+pub struct RobotPath(pub Vec<Isometry2<f64>>);
+
+impl RobotPath {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn clear(&mut self) {
+        self.0.clear();
+    }
+
+    pub fn push(&mut self, pose: Isometry2<f64>) {
+        self.0.push(pose);
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct NavigationRobotPath {
+    local_path: RobotPath,
+    global_path: RobotPath,
+    user_defined_path: HashMap<String, RobotPath>,
+}
+
+impl NavigationRobotPath {
+    pub fn new(local_path: RobotPath, global_path: RobotPath) -> Self {
+        Self {
+            local_path,
+            global_path,
+            user_defined_path: Default::default(),
+        }
+    }
+
+    pub fn set_local_path(&mut self, local_path: RobotPath) {
+        self.local_path = local_path;
+    }
+
+    pub fn set_global_path(&mut self, global_path: RobotPath) {
+        self.global_path = global_path;
+    }
+
+    pub fn local_path(&self) -> &RobotPath {
+        &self.local_path
+    }
+
+    pub fn local_path_mut(&mut self) -> &mut RobotPath {
+        &mut self.local_path
+    }
+
+    pub fn global_path(&self) -> &RobotPath {
+        &self.global_path
+    }
+
+    pub fn global_path_mut(&mut self) -> &mut RobotPath {
+        &mut self.global_path
+    }
+
+    pub fn add_user_defined_path(&mut self, name: &str, path: RobotPath) {
+        self.user_defined_path.insert(name.to_owned(), path);
+    }
+
+    pub fn get_user_defined_path(&self, key: &str) -> Option<&RobotPath> {
+        self.user_defined_path.get(key)
+    }
+}
